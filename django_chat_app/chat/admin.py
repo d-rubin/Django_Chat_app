8e6bdabb8182a53ae8fc40938c25e8cc65acd1a5
Daniel Rubin <daniel.rubin1997@gmail.com>
2023-02-01 23:00:51 +0100

    videos

--- django_chat_app/chat/admin.py
+++ django_chat_app/chat/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Message, Chat
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat', 'text', 'created_at', 'author', 'receiver')
+    list_display = ('text', 'created_at', 'author', 'receiver')
+    search_fields = ('text',)
+
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
+
--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Chat, Message
 
-# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        my_chat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['text'],
+                               chat=my_chat, author=request.user,
+                               receiver=request.user)
+    chat_messages = Message.objects.filter(chat__id=1)
+
+    return render(request, 'chat/index.html', {'username': request.user.first_name, 'messages': chat_messages})
